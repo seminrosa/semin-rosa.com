5623d691f061dabfc68e32425e5062c11e5a0367
William Hansen <will.hansen@gmail.com>
2023-09-18 14:19:13 -0500

    fix header types

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,9 @@
 import { Container } from '@mui/material'
 import headerStyles from './header.module.css'
+import { PropsWithChildren, ReactNode } from 'react'
 
-export default function Header({children}) {
+export default function Header(props: PropsWithChildren) {
     return (
-        <header className={headerStyles.header}>{children}</header>
+        <header className={headerStyles.header}>{props.children}</header>
     )
 }
