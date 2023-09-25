c895575225d5520e1e86dfc2c253f6e4cd5bb0e5
William Hansen <will.hansen@gmail.com>
2023-09-25 13:57:51 -0500

    add basic teaching page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,33 +3,23 @@ import Image from "next/image";
 
 import baseStyle from '../page.module.css';
 import style from './page.module.css';
-import seminYellow1 from '../../../public/semin_yellow_1.jpg';
-import Link from "next/link";
+import seminCommencement1 from '../../../public/semin_commencement_1.jpg';
 
-const aboutText = `This is a page about Semin Park. Add more details here. Keep adding details. This goes on and on and on and on and on on and on and on and on and on on and on and on and on and on on and on and on and on and on on and on and on and on and on on and on and on and on and on on and on and on and on and on.
-Test a new line
-
-
-
-And a few extra lines.
-
-
-`
+const aboutText = `This is the about page.`
 
 export default function About() {
     return (
         <Box className={baseStyle.main}>
             <Grid container>
-                <Grid item xs={6}>
-                    <div className={style.imageWrapper}>
-                        <Image className={style.image} src={seminYellow1} alt={"semin_yellow"} />
-                    </div>
-                </Grid>
                 <Grid item xs>
                     <Typography variant="body1" component="span">
                         <pre style={{fontFamily: 'inherit', whiteSpace: 'pre-wrap'}}>{aboutText}</pre>
                     </Typography>
-                    <Link href="/CV.pdf">View CV</Link>
+                </Grid>
+                <Grid item xs={6}>
+                    <div className={style.imageWrapper}>
+                        <Image className={style.image} src={seminCommencement1} alt={"semin_yellow"} height="400" />
+                    </div>
                 </Grid>
             </Grid>
         </Box>
